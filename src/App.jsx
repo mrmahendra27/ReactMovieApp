7167787a004c3477cc82f286f3919632121b7550
Mahendra Singh Khati <60776546+mrmahendra27@users.noreply.github.com>
2022-03-13 19:21:58 +0530

    added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./Home";
 import { Route, Routes } from "react-router-dom";
 import MovieDeatils from "./MovieDetails";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
   return (
     <div className="app">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<MovieDeatils />} />
+        <Route path="/" element={<Home isLoading={isLoading} setIsLoading={setIsLoading} />} />
+        <Route path="/movie/:id" element={<MovieDeatils isLoading={isLoading} setIsLoading={setIsLoading} />} />
       </Routes>
     </div>
   );
--- src/Home.jsx
+++ src/Home.jsx
@@ -5,15 +5,17 @@ import MovieCard from "./MovieCard";
 
 const API_URL = "https://www.omdbapi.com?apikey=3fbeac48";
 
-const Home = () => {
+const Home = ({ isLoading, setIsLoading }) => {
   const [searchInput, setSearchInput] = useState("");
   const [movieLists, setMovieLists] = useState([]);
 
   const fetchMovies = async (title) => {
+    setIsLoading(true);
     const response = await axios.get(`${API_URL}&s=${title}`);
 
     setSearchInput("");
     setMovieLists(response.data.Search);
+    setIsLoading(false);
   };
 
   const handleKeyDown = (event) => {
@@ -28,33 +30,39 @@ const Home = () => {
 
   return (
     <>
-      <h1>Movie World</h1>
-
-      <div className="search">
-        <input
-          type="text"
-          placeholder="Search for movies.."
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-        <img
-          src={SearchIcon}
-          alt="search"
-          onClick={() => fetchMovies(searchInput)}
-        />
-      </div>
-
-      {movieLists?.length > 0 ? (
-        <div className="container">
-          {movieLists.map((movie, index) => (
-            <MovieCard key={index} movie={movie} />
-          ))}
-        </div>
+      {isLoading == true ? (
+        <div className="loader"></div>
       ) : (
-        <div className="empty">
-          <h2>No movies found</h2>
-        </div>
+        <>
+          <h1>Movie World</h1>
+
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Search for movies.."
+              value={searchInput}
+              onChange={(e) => setSearchInput(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+            <img
+              src={SearchIcon}
+              alt="search"
+              onClick={() => fetchMovies(searchInput)}
+            />
+          </div>
+
+          {movieLists?.length > 0 ? (
+            <div className="container">
+              {movieLists.map((movie, index) => (
+                <MovieCard key={index} movie={movie} />
+              ))}
+            </div>
+          ) : (
+            <div className="empty">
+              <h2>No movies found</h2>
+            </div>
+          )}
+        </>
       )}
     </>
   );
